e842325077fdbc2a280f60b2d37050dd04db32d9
Kevin Gu <guk@aurora.tech>
2021-09-27 21:16:00 +0000

    zip

--- .buildkite/steps/build-lambda.sh
+++ .buildkite/steps/build-lambda.sh
@@ -2,6 +2,7 @@
 set -eu
 
 GOCACHE=/tmp go build -ldflags="-s -w -X version.Build=${BUILDKITE_BUILD_NUMBER}" -o ./lambda/handler ./lambda
+zip -9 -v -j $@ "$<"
 
 # set a version for later steps
 buildkite-agent meta-data set version \
