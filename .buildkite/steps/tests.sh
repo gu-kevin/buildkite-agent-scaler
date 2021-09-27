c03cbd2d1a02a06cf9a06e0762ca2d9005de30b4
Kevin Gu <guk@aurora.tech>
2021-09-27 20:29:38 +0000

    Test

--- .buildkite/steps/tests.sh
+++ .buildkite/steps/tests.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 echo "Get user id"
 echo $UID
 
-GO111MODULE=off go get gotest.tools/gotestsum
+ls -la /go/src
+
+GO111MODULE=off GOCACHE=/tmp go get gotest.tools/gotestsum
 
 echo '+++ Running tests'
 gotestsum --junitfile "junit-${OSTYPE}.xml" -- -count=1 -failfast ./...
