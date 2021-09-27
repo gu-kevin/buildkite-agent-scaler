f00bada09da4a8311a50aa169435a20a70fdb0ae
Kevin Gu <guk@aurora.tech>
2021-09-27 22:02:42 +0000

    Remove duplicate

--- .buildkite/steps/build-lambda.sh
+++ .buildkite/steps/build-lambda.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-set -eux
+set -eu
 
 make handler.zip
-buildkite-agent artifact upload handler.zip
 
+# set a version for later steps
+buildkite-agent meta-data set version \
+  "$(awk -F\" '/const Version/ {print $2}' version/version.go)"
